33f6fd599d3627e2b4b37cbba9c01e99272ff6d2
crazyjin7 <crazyjin7@33b3ec58-7acb-dcd8-1287-7da773661bbe>
2010-05-25 01:51:34 +0000

    getter, setter (old Objective-C Style)

--- CarParts/Classes/Tire.h
+++ CarParts/Classes/Tire.h
@@ -9,8 +9,20 @@
 #import <Foundation/Foundation.h>
 
 
-@interface Tire : NSObject <NSCopying> {
-
+@interface Tire : NSObject {
+	float pressure;
+	float threadDepth;
 }
+
+- (id) initWithPressure:(float)newPressure;
+- (id) initWithThreadDepth:(float)newThreadDepth;
+- (id) initWIthPressure:(float)newPressure
+			threadDepth:(float)newThreadDepth;
+
+- (float) pressure;
+- (void) setPressure:(float)newPressure;
+
+- (float) threadDepth;
+- (void) setThreadDepth:(float)newThreadDepth;
 
 @end
--- CarParts/Classes/Tire.m
+++ CarParts/Classes/Tire.m
@@ -15,4 +15,59 @@
 {
 	return @"I am a tire, I last a while";
 }
+
+- (id) init
+{
+	self = [self initWithPressure:34 threadDepth:20];
+	return self;
+}
+
+- (id) initWithPressure:(float)newPressure
+{
+	self = [super init];
+	if (self != nil) {
+		pressure = newPressure;
+		//self.pressure = newPressure;
+		threadDepth = 20.0;
+	}
+	return self;
+}
+- (id) initWithThreadDepth:(float)newThreadDepth
+{
+	self = [super init];
+	if (self != nil) {
+		pressure = 34.0;
+		threadDepth = newThreadDepth;
+	}
+	return self;
+}
+- (id) initWIthPressure:(float)newPressure
+			threadDepth:(float)newThreadDepth
+{
+	self = [super init];
+	if (self != nil) {
+		pressure = newPressure;
+		threadDepth = threadDepth;
+	}
+	return self;
+}
+
+- (float) pressure
+{
+	return pressure;
+}
+- (void) setPressure:(float)newPressure
+{
+	pressure = newPressure;
+}
+
+- (float) threadDepth
+{
+	return threadDepth;
+}
+- (void) setThreadDepth:(float)newThreadDepth
+{
+	threadDepth = newThreadDepth;
+}
+
 @end
